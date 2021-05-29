f2debb051065ef8ecd877cf1162137424cafe142
majoseek <matuslaw.mati@gmail.com>
2021-05-29 18:35:09 +0200

    Dockerized app

--- src/task/task.database.ts
+++ src/task/task.database.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 const database = new Sequelize("postgres", "postgres", "admin", {
-    host: "localhost",
+    host: "db",
     dialect: "postgres",
     logging: false,
 });
